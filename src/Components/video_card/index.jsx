0fec74b6fd60597ce91d6d79983f5a0abaa66fc1
no_name <ahmedovyhlas001@gmail.com>
2023-12-11 15:03:34 +0300

    last_change

--- src/Components/video_card/index.jsx
+++ src/Components/video_card/index.jsx
@@ -8,9 +8,12 @@ const Video_card = () => {
     return (
         <div className='card p-0 m-0'>
             <div className="card-header">
-                <video width="100%" height="500" controls >
+                <video width="100%" controls >
                     <source src={video} type={'video/mp4'} />
                 </video>
+            </div>
+            <div className="card-body">
+                video one
             </div>
         </div>
     )
